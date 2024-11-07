7347a5dd1f91459d20be483f0d31f9d950c4b40a
kondal08 <konstantinosd08@gmail.com>
2024-11-07 15:20:46 +0000

    Current version but with calling superstructure for all non-drive commands

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -35,27 +35,14 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.Config.Controllers;
 import frc.robot.OI.DriverMap;
 import frc.robot.OI.OperatorMap;
 import frc.robot.commands.DriveCommands;
-import frc.robot.subsystems.climber.ClimberIOReal;
-import frc.robot.subsystems.climber.ClimberIOSim;
-import frc.robot.subsystems.climber.ClimberSubsystem;
-import frc.robot.subsystems.flywheel.FlywheelIOSim;
-import frc.robot.subsystems.flywheel.FlywheelIOSpark;
-import frc.robot.subsystems.flywheel.FlywheelSubsystem;
-import frc.robot.subsystems.intake.IntakeIOReal;
-import frc.robot.subsystems.intake.IntakeIOSim;
-import frc.robot.subsystems.intake.IntakeSubsystem;
-import frc.robot.subsystems.leds.LEDIOPWM;
-import frc.robot.subsystems.leds.LEDIOSim;
-import frc.robot.subsystems.leds.LEDSubsystem;
-import frc.robot.subsystems.pivot.PivotIOReal;
-import frc.robot.subsystems.pivot.PivotIOSim;
-import frc.robot.subsystems.pivot.PivotSubsystem;
+import frc.robot.subsystems.Superstructure;
 import frc.robot.subsystems.prototypes.Prototypes;
 import frc.robot.subsystems.prototypes.Prototypes.PrototypeMotor;
 import frc.robot.subsystems.swerve.GyroIO;
@@ -81,40 +68,7 @@ public class RobotContainer {
               : new SwerveSubsystem(new GyroIO() {}, getSimModules()))
           : null;
 
-  private final PivotSubsystem pivot =
-      Subsystems.PIVOT_ENABLED
-          ? (MODE == RobotMode.REAL
-              ? new PivotSubsystem(new PivotIOReal())
-              : new PivotSubsystem(new PivotIOSim()))
-          : null;
-
-  private final FlywheelSubsystem flywheel =
-      Subsystems.SHOOTER_ENABLED
-          ? (MODE == RobotMode.REAL
-              ? new FlywheelSubsystem(new FlywheelIOSpark())
-              : new FlywheelSubsystem(new FlywheelIOSim()))
-          : null;
-
-  private final IntakeSubsystem intake =
-      Subsystems.INTAKE_ENABLED
-          ? (MODE == RobotMode.REAL
-              ? new IntakeSubsystem(new IntakeIOReal())
-              : new IntakeSubsystem(new IntakeIOSim()))
-          : null;
-
-  private final ClimberSubsystem climber =
-      Subsystems.CLIMBER_ENABLED
-          ? (MODE == RobotMode.REAL
-              ? new ClimberSubsystem(new ClimberIOReal())
-              : new ClimberSubsystem(new ClimberIOSim()))
-          : null;
-
-  private final LEDSubsystem leds =
-      Subsystems.LEDS_ENABLED
-          ? (MODE == RobotMode.REAL
-              ? new LEDSubsystem(new LEDIOPWM())
-              : new LEDSubsystem(new LEDIOSim()))
-          : null;
+  private final Superstructure superstructure = new Superstructure(drive::getPose);
 
   private final Prototypes prototypes =
       Subsystems.PROTOTYPES_ENABLED ? new Prototypes(new PrototypeMotor(1, "Motor 1")) : null;
@@ -144,25 +98,12 @@ public class RobotContainer {
     autoChooser.addOption(
         "Drive SysId (Dynamic Reverse)", drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
 
-    autoChooser.addOption(
-        "Flywheel SysId (Quasistatic Forward)",
-        flywheel.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Flywheel SysId (Quasistatic Reverse)",
-        flywheel.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
-    autoChooser.addOption(
-        "Flywheel SysId (Dynamic Forward)", flywheel.sysIdDynamic(SysIdRoutine.Direction.kForward));
-    autoChooser.addOption(
-        "Flywheel SysId (Dynamic Reverse)", flywheel.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+    superstructure.registerSuperstructureCharacterization(() -> autoChooser);
   }
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Set up auto routines
-    registerCommand(
-        "Run Flywheel",
-        startEnd(() -> flywheel.runVelocity(flywheelSpeedInput.get()), flywheel::stop, flywheel)
-            .withTimeout(5.0));
     autoChooser = new LoggedDashboardChooser<>("Auto Choices", AutoBuilder.buildAutoChooser());
 
     registerCharacterization();
@@ -192,31 +133,13 @@ public class RobotContainer {
                                 .getDistance(SPEAKER.getPose().getTranslation())
                             < Units.feetToMeters(25))
                 .and(driver.alignToSpeaker()))
-        .whileTrue(
-            pivot.run(
-                () ->
-                    pivot.setPosition(
-                        () ->
-                            Math.atan2(
-                                2.1,
-                                drive
-                                    .getPose()
-                                    .getTranslation()
-                                    .getDistance(SPEAKER.getPose().getTranslation())))));
-
-    pivot.setDefaultCommand(pivot.run(() -> pivot.setPosition(() -> 0)));
-
-    leds.setDefaultCommand(
-        leds.setRunAlongCmd(
-            () -> AllianceFlipUtil.shouldFlip() ? Color.kRed : Color.kBlue,
-            () -> Color.kBlack,
-            5,
-            1));
+        .onTrue(superstructure.setSuperStateCmd(Superstructure.SuperStates.SHOOTING))
+            .onFalse(superstructure.setSuperStateCmd(Superstructure.SuperStates.IDLING));
 
     driver.testButton().onTrue(driver.rumble().withTimeout(1.0));
 
     new Trigger(() -> Math.abs(drive.getNoteOffset()) < 5 && drive.getNoteOffset() != 0)
-        .whileTrue(leds.setBlinkingCmd(Color.kYellow, Color.kBlack, 5));
+        .whileTrue(superstructure.setLEDBlinkingCmd(Color.kYellow, Color.kBlack, 5));
 
     GamePieceVisualizer.setRobotPoseSupplier(drive::getPose);
     GamePieceVisualizer.setTargetSupplier(
@@ -229,8 +152,8 @@ public class RobotContainer {
                 new Rotation3d()));
     operator
         .shoot()
-        .and(new Trigger(() -> flywheel.getVelocityRPM() > 1200))
-        .onTrue(GamePieceVisualizer.shoot(() -> 0.5, pivot::getPosition));
+        .and(superstructure.shooterVelocityGreater())
+        .onTrue(superstructure.setSuperStateCmd(Superstructure.SuperStates.SHOOT));
   }
 
   private void registerDrivetrain() {
@@ -299,9 +222,8 @@ public class RobotContainer {
     if (SHOOTER_ENABLED && OPERATOR_ENABLED) {
       operator
           .shoot()
-          .whileTrue(
-              startEnd(
-                  () -> flywheel.runVelocity(flywheelSpeedInput.get()), flywheel::stop, flywheel));
+          .onTrue(superstructure.setSuperStateCmd(Superstructure.SuperStates.SHOOT))
+                  .onFalse(superstructure.setSuperStateCmd(Superstructure.SuperStates.IDLING));
     }
   }
 
